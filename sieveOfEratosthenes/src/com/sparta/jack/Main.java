e3dc32c83434ff9f837b9262795f68b6af889da0
jackingham <jackingham1998@gmail.com>
2021-03-25 23:52:02 +0000

    add comments for code clarity

--- sieveOfEratosthenes/src/com/sparta/jack/Main.java
+++ sieveOfEratosthenes/src/com/sparta/jack/Main.java
@@ -1,15 +1,14 @@
 package com.sparta.jack;
 
-import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String args[]) {
-        int upperBound = 100;
+    public static void main(String[] args) {
+        int upperBound = 5000;
         boolean[] booleanArray = SieveClass.sieveOfEratosthenes(upperBound);
         System.out.println("Final number list");
-        for (int i = 2; i <= upperBound; i++) {
-            if (booleanArray[i] == true)
+        for (int i = 2; i <= upperBound; i++) { //starts from 2 as 1 is a special case and not prime
+            if (booleanArray[i])
                 System.out.print(i + " ");
         }
     }
--- sieveOfEratosthenes/src/com/sparta/jack/SieveClass.java
+++ sieveOfEratosthenes/src/com/sparta/jack/SieveClass.java
@@ -4,18 +4,18 @@ public class SieveClass {
 
     public static boolean[] sieveOfEratosthenes(int upperBound) {
 
-        boolean primeIndexArray[] = new boolean[upperBound + 1];
+        boolean[] primeIndexArray = new boolean[upperBound + 1];
         for (int i = 0; i < upperBound+1; i++)
             primeIndexArray[i] = true;
 
-        for (int n= 2; n* n<= upperBound; n++) {
-            if (primeIndexArray[n] == true) {
+        for (int n= 2; n* n<= upperBound; n++) {  //starts from 2 as 1 is a special case and not prime
+            if (primeIndexArray[n]) {
                 System.out.println("Prime found:" +n+"\n");
                 for (int j = n*n; j <= upperBound; j += n) {
                     primeIndexArray[j] = false;
                     System.out.println(j+" is divisible by "+ n+" so is removed");
                     for (int i = 2; i <= upperBound; i++) {
-                        if (primeIndexArray[i] == true)
+                        if (primeIndexArray[i])
                             System.out.print(i + " ");
                     }
                     System.out.println("\n");
